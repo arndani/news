4f12c88762f2b1812fbbf776e80d4b24a1e87eef
arndani <dan.arnold328@gmail.com>
2020-02-24 02:06:07 +0000

    pushagain

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 require "geocoder"
+require "forecast_io"
+
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
-<<<<<<< HEAD
-   # url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=00daa4473b72423cbee7e2e223f861ac"
-    #news = HTTParty.get(url).parsed_response.to_hash
+    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=00daa4473b72423cbee7e2e223f861ac"
+    news = HTTParty.get(url).parsed_response.to_hash
 
+ ForecastIO.api_key = "599ea1f85ac191f31ea5460abe64d084"
 
+ 
 get "/" do
 
      #url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=00daa4473b72423cbee7e2e223f861ac"
@@ -20,37 +23,18 @@ view "ask"
 end
 
 get "/news" do
-    results = Geocoder.search(params["q"])
+  results = Geocoder.search(params["q"])
     lat_long = results.first.coordinates # => [lat, long]
-   @lat =lat_long[0]} 
-    @long-lat_long[1]
+    @lat_long="#{lat_long[0]} #{lat_long[1]}"
+
+ forecast = ForecastIO.forecast(90, 90).to_hash
+
+
 end
 
-#Shows the news output
 
   
     
    # view "ask"
 
     #results = Geocoder.search("Evanston, IL")
-=======
-# enter your Dark Sky API key here
-ForecastIO.api_key = "043b254525084c12a2377cedb26aa925"
-
-
-# news is now a Hash you can pretty print (pp) and parse for your output
-#pp news
-
-get "/" do
-    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=043b254525084c12a2377cedb26aa925"
-news = HTTParty.get(url).parsed_response.to_hash
-
-    @news=news
-view "ask"
-  # show a view that asks for the location
-end
-
-#get "/news" do
-  # do everything else
-#end
->>>>>>> 1b102d57b633b3abc8930c3e410be35871d4bafa
