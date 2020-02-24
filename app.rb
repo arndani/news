8346f10740fab02b651208debf0625804c4c2528
arndani <dan.arnold328@gmail.com>
2020-02-24 01:27:34 +0000

    pushme

--- app.rb
+++ app.rb
@@ -1,18 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
 require "geocoder"
-require "forecast_io"
-require "httparty"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
-# enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+   # url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=00daa4473b72423cbee7e2e223f861ac"
+    #news = HTTParty.get(url).parsed_response.to_hash
+
 
 get "/" do
-  # show a view that asks for the location
+
+     #url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=00daa4473b72423cbee7e2e223f861ac"
+    #news = HTTParty.get(url).parsed_response.to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
+
+     #pp news
+view "ask"
 end
 
 get "/news" do
-  # do everything else
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => [lat, long]
+   @lat =lat_long[0]} 
+    @long-lat_long[1]
 end
+
+#Shows the news output
+
+  
+    
+   # view "ask"
+
+    #results = Geocoder.search("Evanston, IL")
